6b2227af7a6ac65f71699ae6f9c4d2dcd0377b98
userBotN1 <lzhyang2000@gmail.com>
2024-01-12 22:51:07 -0500

    Trend page updates

---
+++ trend/trend.js
@@ -0,0 +1,6 @@
+// const left_btn = document.querySelector(".change-date-btn");
+// const right_btn = document.querySelector(".customize-btn");
+
+// left_btn.addEventListener("click", () => console.log("left clicked"));
+
+// right_btn.addEventListener("click", () => console.log("right clicked"));
