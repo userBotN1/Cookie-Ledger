a0686c36d052069d746f4df5c93c327d591b3ec1
userBotN1 <lzhyang2000@gmail.com>
2024-02-27 20:55:14 -0500

    display and hide calculator on user click

--- add/add.js
+++ add/add.js
@@ -25,6 +25,10 @@ class UI {
       categoryTitle: document.querySelector(".header__title"),
       leftBtn: document.querySelector(".header__left-btn"),
       rightBtn: document.querySelector(".header__right-btn"),
+      expenditureDiv: document.querySelector(".main-container__expenditure"),
+      incomeDiv: document.querySelector(".main-container__income"),
+      calculatorContainer: document.querySelector(".modal-container"),
+      overlay: document.querySelector(".overlay"),
     };
   }
 
@@ -113,6 +117,31 @@ ui.doms.leftBtn.addEventListener("click", prevCategory);
 ui.doms.rightBtn.addEventListener("click", nextCategory);
 document.addEventListener("DOMContentLoaded", initializeCategory);
 
-// main_container__income;
-// main_conatiner__expenditure;
+/**
+ * Selecting a category and open up the calculator
+ */
+
+function closeCalculator() {
+  ui.doms.calculatorContainer.classList.add("hidden");
+  ui.doms.overlay.classList.add("hidden");
+}
+function processRecord(category) {
+  console.log(category);
+  ui.doms.calculatorContainer.classList.remove("hidden");
+  ui.doms.overlay.classList.remove("hidden");
+  console.log(ui.doms.calculatorContainer);
+}
+
+function initializeRecord(event) {
+  let clickArea = event.target.tagName;
+  if (clickArea === "SPAN" || clickArea === "BUTTON") {
+    clickArea = event.target.closest(".category-container__btn");
+    const category = clickArea.nextElementSibling.textContent;
+    processRecord(category);
+  }
+}
+
+ui.doms.expenditureDiv.addEventListener("click", initializeRecord);
+ui.doms.incomeDiv.addEventListener("click", initializeRecord);
+ui.doms.overlay.addEventListener("click", closeCalculator);
 
